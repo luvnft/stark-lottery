cbb58017cf27c003e2da12cf07e5b7467084bf2d
karasfantom <karasdeveloper@gmail.com>
2024-03-03 08:49:26 +0700

    [update] struct

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: {
+                loader: '@svgr/webpack',
+                options: {
+                    svgo: true,
+
+                    svgoConfig: {
+                        multipass: true,
+                        plugins: [
+                            {
+                                name: 'prefixIds',
+                                params: {
+                                    overrides: {
+                                        removeViewBox: false,
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                },
+            },
+        });
+        return config;
+    },
+};
 
 export default nextConfig;
---
+++ providers/ChakraProvider.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChakraProvider = () => {
+  return <div>ChakraProvider</div>;
+};
+
+export default ChakraProvider;
