f5469ed778a6c785c227f4879dd759754958df8b
kafierence <vannam34760@gmail.com>
2024-05-12 17:21:47 +0700

    [update] new white list content

--- components/Header/ListPageItem.tsx
+++ components/Header/ListPageItem.tsx
@@ -58,7 +58,7 @@ const ListPageItem = ({ sx }: IProps) => {
           </Link>
         );
       })}
-      <Link href="https://decolgen.gitbook.io/stark-arcade-doc" target="_blank">
+      <Link href="https://www.starkarcade.com" target="_blank">
         <Text
           px={{ lg: 8, md: 4 }}
           py={2}
@@ -66,7 +66,7 @@ const ListPageItem = ({ sx }: IProps) => {
           color={'white'}
           borderBottomColor="#119EF9"
         >
-          GitBook
+          StarkArcade
         </Text>
       </Link>
     </HStack>
--- providers/index.tsx
+++ providers/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
 'use client';
 import { PropsWithChildren } from 'react';
 import ProviderChakra from './ProviderChakra';
 import ProviderScript from './ProviderScript';
 import ProviderStarknet from './ProviderStarknet';
 
+// eslint-disable-next-line react/display-name
 export default ({ children }: PropsWithChildren) => {
   return (
     <>
