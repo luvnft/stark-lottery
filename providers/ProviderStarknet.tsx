69338388651475d1a5b45eb59b7915009d2f3db4
kafierence <vannam34760@gmail.com>
2024-10-11 23:36:47 +0700

    [fix] lottery

--- providers/ProviderStarknet.tsx
+++ providers/ProviderStarknet.tsx
@@ -7,13 +7,15 @@ import {
 } from '@starknet-react/core';
 import React, { PropsWithChildren } from 'react';
 
-import { ArgentMobileConnector } from 'starknetkit/argentMobile';
+import {
+  ArgentMobileBaseConnector,
+  ArgentMobileConnector,
+} from 'starknetkit/argentMobile';
 
 const ProviderStarknet = ({ children }: PropsWithChildren) => {
   const connectors = [
     new InjectedConnector({ options: { id: 'argentX', name: 'Argent' } }), // Suggest Injected Conenctor
     new InjectedConnector({ options: { id: 'braavos', name: 'Braavos' } }),
-    new ArgentMobileConnector(), // Mobile Scan Argent Working - Display Type Modal
   ];
   function rpc(chain: Chain) {
     return {
